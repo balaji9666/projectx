7ca00e1ea0b3741aa15b0c7860a34a603bd0e6fb
balaji9666 <58804855+balaji9666@users.noreply.github.com>
2019-12-12 12:55:11 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb://localhost:27017/projectx",
+  //mongodb://bala_9666:Msbalaji5235@cluster0-20ia5.mongodb.net/test
+  secretOrKey: "secret"
+};
---
+++ validation/register.js
@@ -0,0 +1,49 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+  // Convert empty fields to an empty string so we can use validator functions
+  data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+  data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  data.mobileNo = !isEmpty(data.mobileNo) ? data.mobileNo : "";
+  // Name checks
+  if (Validator.isEmpty(data.firstName)) {
+    errors.name = "First Name field is required";
+  }
+  if (Validator.isEmpty(data.lastName)) {
+    errors.name = "Last Name field is required";
+  }
+  // Email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  // Mobile checks
+  if (Validator.isEmpty(data.mobileNo)) {
+    errors.mobileNo = "Mobile No field is required";
+  }
+  if (!Validator.isLength(data.mobileNo, { min: 10, max: 10 })) {
+    errors.mobileNo = "Mobile Number must be 10 digit number";
+  }
+  // Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
